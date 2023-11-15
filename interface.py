70b5cef4a21985b441381e37deec57ac71cb53e5
Klee057 <83681550+Klee057@users.noreply.github.com>
2023-11-15 17:00:01 +0800

    dynamic tree interface

--- interface.py
+++ interface.py
@@ -73,8 +73,6 @@ select
         # Create two buttons
         self.ExecuteBtn = tk.Button(self.root, text="Execute", command=self.processQuery)
         # self.clearBtn = tk.Button(root, text="Clear Input", command=self.clearText)
-        scrollbar_x = tk.Scrollbar(self.root, orient=tk.HORIZONTAL)
-        self.text_widget = tk.Text(self.root, height=50, width=110, wrap=tk.NONE,xscrollcommand=scrollbar_x.set)
         ############################################## label input
         self.label.pack(side=tk.TOP,pady=10)
         self.text.pack(pady=10)
@@ -83,27 +81,73 @@ select
         
         ################################################## tree
       # Create a text widget to display the tree structure
-        scrollbar_x.pack(side=tk.BOTTOM, fill=tk.X)
-        scrollbar_x.config(command=self.text_widget.xview)
-        self.text_widget.pack()
+        # self.text_widget.pack()
         
         # button2.pack(pady=5)
+
+  def draw_tree(self, canvas, node, x, y, x_spacing=250, y_spacing=100):
+      canvas.create_rectangle(x - 100, y - 30, x + 100, y + 30)
+      if node.relation:
+          canvas.create_text(x, y-20, text=node.nodeType)
+          canvas.create_text(x, y, text=f'Relation: {node.relation}')
+          canvas.create_text(x, y+20, text=f'Cost: {node.cost}')
+      elif node.condition:
+          canvas.create_text(x, y-20, text=node.nodeType)
+          canvas.create_text(x, y, text=f'Condition: {node.condition}')
+          canvas.create_text(x, y+20, text=f'Cost: {node.cost}')
+      elif node.cost:
+          canvas.create_text(x, y-20, text=node.nodeType)
+          canvas.create_text(x, y, text=f'Cost: {node.cost}')
+      else:
+          canvas.create_text(x, y, text=node.nodeType)
+  
+      child_y = y + y_spacing
+      for child in node.children:
+          if len(node.children) == 1:
+              child_x = x
+          else:
+              child_x = x + x_spacing * (node.children.index(child) - len(node.children) / 4) * (node.level/3)
+          canvas.create_line(x, y + 30, child_x, child_y - 30, fill="black")
+          self.draw_tree(canvas, child, child_x, child_y, x_spacing, y_spacing)
+  
+  def create_tree(self,result):
+      node = QueryPlanNode(nodeType=result["Node Type"])
+  
+      if result["Node Type"] == "Hash Join":
+          node.condition = result["Hash Cond"]
+          node.cost = result["Total Cost"]
+  
+      elif result["Node Type"] == "Index Scan":
+          node.cost = result["Total Cost"]
+          
+      elif result["Node Type"] == "Hash":
+          node.cost = result["Total Cost"]
+          
+  
+      elif result["Node Type"] == "Seq Scan":
+          node.relation = result["Relation Name"]
+          if "Filter" in result.keys():
+            node.condition = result["Filter"]
+          node.cost = result["Total Cost"]
+      
+      if "Plans" in result.keys():
+          for child in result["Plans"]:
+              expanded_child = self.create_tree(child)
+              node.children.append(expanded_child)
+              if node.level < expanded_child.level + 1:
+                  node.level = expanded_child.level + 1
+  
+      return node
+
 
   def executeQuery(self ,query):
       self.label.config(text="wait abit pls...")
       explainQuery = "explain (analyze, costs, verbose, buffers, format json)\n" + query
       self.data = qep.retrieve_query_plan(self.db_conn, explainQuery)
       
-      # Create the tree
-      # Create the root node
-    #   self.data = json.loads(json_data)
-      root = Node("Query Plan")
-      self.create_tree(self.data, parent=root)
-      self.label.config(text="Executed!")
 
-      self.text_widget.delete(1.0, tk.END)  # Clear previous content
-      for pre, _, node in RenderTree(root):
-        self.text_widget.insert(tk.END, "%s%s\n" % (pre, node.name))
+
+      return self.data
 
       # Print the tree structure
       # for pre, fill, node in RenderTree(root):
@@ -123,37 +167,61 @@ select
     
   def start(self):
       # Start the Tkinter event loop
+
+
+
       self.root.mainloop()
 
 
   def processQuery(self):
       # label.config(text="Button 2 Clicked!")
       qry = self.text.get("1.0", tk.END).strip()
-      self.executeQuery(qry)
+
+      # Create a canvas and link it with the scrollbar
+
+    #   frame=tk.Frame(self.root,width=1000,height=100)
+    #   frame.pack(expand=True, fill=tk.BOTH) #.grid(row=0,column=0)
+    #   canvas=tk.Canvas(frame,bg='#FFFFFF',width=1000,height=200,scrollregion=(-500,-100,500,2000))
+    # Create scrollbars for both x and y axes
+      scrollbar_y = tk.Scrollbar(self.root, orient="vertical")
+      scrollbar_y.pack(side="right", fill="y")
+
+      scrollbar_x = tk.Scrollbar(self.root, orient="horizontal")
+      scrollbar_x.pack(side="bottom", fill="x")
+
+    # Create a canvas and link it with the scrollbars
+      canvas = tk.Canvas(self.root, xscrollcommand=scrollbar_x.set, yscrollcommand=scrollbar_y.set)
+      canvas.pack(side="left", fill="both", expand=True)
+
+    # Link scrollbars to canvas
+      scrollbar_x.config(command=canvas.xview)
+      scrollbar_y.config(command=canvas.yview)
+        # Add content to the canvas
+      frame = tk.Frame(canvas)
+      canvas.create_window((0, 0), window=frame, anchor="nw")
+      # Configure the scrollbar to interact with the canvas
+    #   canvas.pack()
+      
+      result = self.executeQuery(qry)
+      plan = result["Plan"]
+  
+      root_node = self.create_tree(plan)
+      self.draw_tree(canvas, root_node, 400, 50)
+
+      frame.update_idletasks()
+      canvas.config(scrollregion=canvas.bbox("all"))
+     # Enable mouse wheel scrolling on the canvas
+    #   canvas.bind_all("<MouseWheel>", self.on_scroll)
+    #   return self.executeQuery(qry)
 
   # def clearText():
   #     text.delete("1.0", tk.END)
   #     label.config(text="Cleared!")
 
+  def on_scroll(self, event):
+      self.canvas.yview_scroll(-1 * (event.delta // 120), "units")
 
 # Function to create tree structure
-  def create_tree(self,data, parent=None):
-      arr = []
-      if isinstance(data, dict):
-          for key, value in data.items():
-              if key == "Plans":
-                  # arr = value
-                  # value = "plan"
-                  node = Node(f'{key}: plan', parent=parent)
-                  self.create_tree(value, parent=node)
-                  
-              else:
-                  node = Node(f'{key}: {value}', parent=parent)
-                  self.create_tree(value, parent=node)
-      elif isinstance(data, list):
-          for item in data:
-              node = Node("", parent=parent)
-              self.create_tree(item, parent=node)
 #########################################################################################
 
 
@@ -298,3 +366,31 @@ json_data = '''
     "Execution Time": 3795.251
 }
     '''
+class QueryPlanNode:
+    def __init__(self, nodeType, cost=None, relation=None, condition=None, children=None):
+        self.nodeType = nodeType
+        self.cost = cost
+        self.relation = relation
+        self.condition = condition
+        self.children = children or []
+        self.level = 1
+
+
+def main(self):
+    root = tk.Tk()
+    root.title("Query Plan Tree")
+
+    canvas = tk.Canvas(root, width=2000, height=2000)
+    canvas.pack()
+    
+    json_string = '''{"Plan": {"Node Type": "Sort", "Plans": [{"Node Type": "Aggregate", "Plans": [{"Node Type": "Gather Merge", "Plans": [{"Node Type": "Aggregate", "Plans": [{"Node Type": "Sort", "Plans": [{"Node Type": "Hash Join", "Hash Cond": "(lineitem.l_orderkey = orders.o_orderkey)", "Total Cost": 197052.4, "Plans": [{"Node Type": "Seq Scan", "Relation Name": "lineitem", "Filter": "(lineitem.l_extendedprice > '10'::numeric)", "Total Cost": 143857.55}, {"Node Type": "Hash", "Parent Relationship": "Inner", "Total Cost": 40128.41, "Hash Buckets": 524288, "Plans": [{"Node Type": "Hash Join", "Hash Cond": "(orders.o_custkey = customer.c_custkey)", "Total Cost": 40128.41, "Plans": [{"Node Type": "Seq Scan", "Relation Name": "orders", "Filter": "(orders.o_totalprice > '10'::numeric)", "Total Cost": 33948.5}, {"Node Type": "Hash", "Parent Relationship": "Inner", "Total Cost": 4381.25, "Hash Buckets": 32768, "Plans": [{"Node Type": "Seq Scan", "Relation Name": "customer", "Filter": "(customer.c_mktsegment = 'BUILDING'::bpchar)", "Total Cost": 4381.25}]}]}]}]}]}]}]}]}]}, "Planning Time": 6.528, "Triggers": [], "Execution Time": 2649.252}'''
+    result = json.loads(json_string)
+    plan = result["Plan"]
+
+    root_node = self.create_tree(plan)
+    draw_tree(canvas, root_node, 400, 50)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
